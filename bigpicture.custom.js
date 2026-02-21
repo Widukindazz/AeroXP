62de88e157dca14493347598d3fa06b67ea90ec6
Numbers <164122080+Sennanon87@users.noreply.github.com>
2026-02-21 11:04:08 -0700

    Add files via upload

---
+++ bigpicture.custom.js
@@ -0,0 +1,13 @@
+/**
+ * @description This js loads in BPM (Big Picture Mode)
+ * 
+ * @note only functions if the user has javascript enabled. 
+ */
+
+/*
+function initialize() {
+    console.log("Hello World from BPM!")
+}
+
+initialize()
+*/
---
+++ libraryroot.custom.js
@@ -0,0 +1,6 @@
+/**
+ * @description This js load in the friends list, chat, and other related areas. 
+ * 
+ * @note only functions if the user has javascript enabled. 
+ */
+ 
